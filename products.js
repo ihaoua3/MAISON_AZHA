e1affa89b2dc380b79f3bcc6e1253d3e0fa332e3
ihaoua3 <ihaoua3@icloud.com>
2026-07-03 14:36:32 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: "Parfum Royal AZHA",
+    price: 15000
+  },
+  {
+    id: 2,
+    name: "Crème Visage Luxe",
+    price: 10000
+  },
+  {
+    id: 3,
+    name: "Kit Beauté Premium",
+    price: 25000
+  },
+  {
+    id: 4,
+    name: "Huile Parfumée Orientale",
+    price: 12000
+  }
+];
